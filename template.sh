a8e4785eef15838a49ece206366ba6b0e66c2ac9
zauberermerlin <datensammelstelle@gmail.com>
2016-11-01 22:18:44 +0100

    Erster Ansatz: Auflistung der grundsätzlichrn Parameter

--- template.sh
+++ template.sh
@@ -1,9 +1,25 @@
 !#/bin/bash
 #
 # Schablone fuer Scripte
+# Grundgeruest zum Start eines Linux Scriptes
 #
+
 VERSION="0.1";
 echo $VERSION
 
-
+# Aufruf-Parameter, die in jedem Script vorhanden sein sollten:
+# -h oder --help: Hilfstext mit Bsp.
+# -install: kopieren und verlinken des Scripts, damit direkter Zugriff erfolgt
+# -remove: den mit -install durchgeführten Vorgang rückgängig machen
+# -check
+#
+# -ansible: Erstellung bzw. Anzeige des Ansible Playbooks
+# -log
+# -test
+#
+# Überpruefung der mitgegebenen Parameter. Ist einer der Parameter
+# -h oder --help oder -install oder -remove vorhanden, dann werden alle
+# anderen Parameter ignoriert
+# Anzahl der Paramter: $#
+# erster Parameter: $1
  
