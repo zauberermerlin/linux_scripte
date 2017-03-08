9fbac8237f7c252b4d91168412f2e16a4e67c5e4
thomas <thomas@mulholland>
2017-03-09 00:07:50 +0100

    Neuanlage: xxx.sh und daten_exiftool.txt Update: template.sh

--- template.sh
+++ template.sh
@@ -17,55 +17,38 @@
 
 
 
-VERSIONSTEXT="Version 0.2 vom 10.12.2016";
-VERSION="0.1";
-echo $VERSION;
+VERSIONSDATUM="08.03.2017";
+VERSION="0.3";
 
 BEGINN_DATUM=$(date +%d.%m.%Y);
-echo $BEGINN_DATUM;
+#echo $BEGINN_DATUM;
 
 LAUFZEIT_BEGINN=$(date +%s);
-echo $LAUFZEIT_BEGINN;
-sleep 2s
+#echo $LAUFZEIT_BEGINN;
+#sleep 2s
 
 LAUFZEIT_ENDE=$(date +%s);
-echo $LAUFZEIT_ENDE;
-echo $LAUFZEIT_BEGINN - $LAUFZEIT_ENDE;
+#echo $LAUFZEIT_ENDE;
+#echo $LAUFZEIT_BEGINN - $LAUFZEIT_ENDE;
 
 
 # Aufruf-Parameter, die in jedem Script vorhanden sein sollten:
 # -h oder --help: Hilfstext mit Bsp.
-# -install: kopieren und verlinken des Scripts, damit direkter Zugriff erfolgt
+# -install: kopieren und verlinken des Scripts, damit direkter Zugriff erfolgt (ggfls. mit -check)
+# 		-check: Verlinkung und chmod, chown Settings
 # -remove: den mit -install durchgeführten Vorgang rückgängig machen
-# -check: Verlinkung und chmod, chown Settings
 # -s: sleep/warten in Minuten und herunterzählen in einer Zeile wieviel Minuten noch zu warten ist
 # -w: nach Beendigung eine Whatsapp versenden
 # -m: nach Beendigung eMail versenden
 # -v oder --version: Ausgabe des Versionsstrings
-# --check:: überprüfen, ob ein Update auf github vorhanden ist
-# --update: neueste Version von github holen und installieren
+# -git: überprüfen, ob ein Update auf github vorhanden ist
+# -update: neueste Version von github holen und installieren
 #
 #
 # -ansible: Erstellung bzw. Anzeige des Ansible Playbooks
 # -log: schreibt ins aktuelle Verzeichnis die Datei log.txt
-# -test
+# -test oder -check
 #
-# Überpruefung der mitgegebenen Parameter. Ist einer der Parameter
-# -h oder --help oder -install oder -remove vorhanden, dann werden alle
-# anderen Parameter ignoriert
-# Anzahl der Paramter: $#
-# erster Parameter: $1
-# alle Argumente in einer Zeichenkette: $*
-# alle Argumente in einem Array: $@
-#
-# for i in "${arrayName[@]}"
-# do
-# do whatever on $i
-# done
-#
-#
-# Länge der Parameterliste:
-# len=${#array[@]}
 
 
 #############################################################
@@ -111,3 +94,45 @@ echo $LAUFZEIT_BEGINN - $LAUFZEIT_ENDE;
 # Parameter: -checkupdate
 #############################################################
 
+
+#############################################################
+# Funktionen definieren
+# muessen vor dem Aufruf definiert werden
+#############################################################
+
+# Hilfe Funktion, zur Ausgabe des Hilfstextes
+# function hilfe_test()
+# {
+# echo "";
+# }
+
+
+#############################################################
+# Auswertung der Parameter
+#############################################################
+
+# Ueberpruefung der mitgegebenen Parameter. Ist einer der Parameter
+# -h oder --help oder -install oder -remove vorhanden, dann werden alle
+# anderen Parameter ignoriert
+# Anzahl der Paramter: $#
+# erster Parameter: $1
+# alle Argumente in einer Zeichenkette: $*
+# alle Argumente in einem Array: $@
+#
+# for i in "${arrayName[@]}"
+# do
+# do whatever on $i
+# done
+#
+#
+# Laenge der Parameterliste:
+# len=${#array[@]}
+
+# case $1 in
+# -h|--h|-help|--help)
+# echo "";
+# echo "";;
+# *)
+# echo "";
+# echo "";;
+# esac
---
+++ xxx.sh
@@ -0,0 +1,185 @@
+#!/bin/bash
+#
+# Programm, dass alles rund um xxx erledigt.
+#
+#
+#
+
+
+
+#######################
+#
+# Offizieller Pfad des Repos'
+# https://github.com/zauberermerlin/linux_scripte.git
+#
+# fuer curl wird der raw Pfad der einzelnen Datei benötigt
+#
+#
+#curl -L https://raw.githubusercontent.com/zauberermerlin/linux_scripte/master/video_ffmpeg >video_ffmpeg.sh
+#
+#######################
+
+
+
+VERSIONSDATUM="08.03.2017";
+VERSION="0.1";
+
+BEGINN_DATUM=$(date +%d.%m.%Y);
+#echo $BEGINN_DATUM;
+
+LAUFZEIT_BEGINN=$(date +%s);
+#echo $LAUFZEIT_BEGINN;
+
+LAUFZEIT_ENDE=$(date +%s);
+#echo $LAUFZEIT_ENDE;
+#echo $LAUFZEIT_BEGINN - $LAUFZEIT_ENDE;
+
+
+#############################################################
+#
+#path-Variable anpassen
+#Datei: .bashrc
+#PATH=$PATH:/home/thomas/script
+#(mit Doppelpunkt)
+#
+#############################################################
+#############################################################
+# Parameter: -install
+#############################################################
+#
+# Datei ist mit dem User 'root' nach /usr/sbin zu kopieren (verlinken geht nicht; warum??? unklar)
+# anschliessend ist das r und x Flag für alle zu setzen.
+# Zum Abschluss testen, ob die Datei dort auch wirklich vorhanden ist
+#
+# sudo cp $0 /usr/sbin 
+#
+#############################################################
+# Parameter: -remove
+#############################################################
+#
+# Datei als User 'root' in /usr/sbin loeschen.
+# Zum Abschluss das "nicht"-Vorhandensein testen
+#
+
+#############################################################
+# Parameter: -update
+#############################################################
+#
+# Offizieller Pfad des Repos'
+# https://github.com/zauberermerlin/linux_scripte.git
+#
+# fuer curl wird der raw Pfad der einzelnen Datei benötigt
+#
+#
+#curl -L https://raw.githubusercontent.com/zauberermerlin/linux_scripte/master/template.sh >template.sh
+#
+
+#############################################################
+# Parameter: -checkupdate
+#############################################################
+
+
+#############################################################
+# Funktionen definieren
+# muessen vor dem Aufruf definiert werden
+#############################################################
+
+#
+# Hilfe Funktion, zur Ausgabe des Hilfstextes
+#
+
+function hilfe_text()
+{
+echo "";
+echo "Hilfe fuer:" $0 "Version:" $VERSION "vom:" $VERSIONSDATUM
+}
+
+#
+# Ueberpr�ft ob und welche Dateien vorhanden sind
+# exiftool
+# 
+# slug.mp4-Datei
+# slug.txt-Datei
+# slug.jpg-Dateien
+#
+
+function programme_dateien_check()
+{
+}
+
+
+
+#############################################################
+# Auswertung der Parameter
+#############################################################
+
+# Ueberpruefung der mitgegebenen Parameter. Ist einer der Parameter
+# -h oder --help oder -install oder -remove vorhanden, dann werden alle
+# anderen Parameter ignoriert
+# Anzahl der Paramter: $#
+# erster Parameter: $1
+# alle Argumente in einer Zeichenkette: $*
+# alle Argumente in einem Array: $@
+#
+# for i in "${arrayName[@]}"
+# do
+# do whatever on $i
+# done
+#
+#
+# Laenge der Parameterliste:
+# len=${#array[@]}
+
+
+#08.03.17
+# Parameter: -t oder --template_erzeugen (ggfls. in Verbindung mit -r und/oder -check)
+# Parameter: -tag_schreiben (ggfls. in Verbindung mit -r und/oder -check)
+# Parameter: -tag_datei (ggfls. in Verbindung mit -r und/oder -check)
+# Parameter: -info
+# Parameter: -check (ggfls. in Verbindung mit -r oder -update)
+# Parameter: -log: log_jjjjmmtt-hhmm.txt-Datei im aktuellen Verzeichnis erzeugen
+# Parameter: -v oder --version
+# -ansible: Erstellung bzw. Anzeige des Ansible Playbooks
+# -install: kopieren und verlinken des Scripts, damit direkter Zugriff erfolgt (ggfls. mit -check)
+# -remove: den mit -install durchgefuehrten Vorgang rueckgaengig machen
+# -w: nach Beendigung eine Whatsapp versenden
+# -m: nach Beendigung eMail versenden
+# -git: Ueberpruefen, ob ein Update auf github vorhanden ist
+# -update: neueste Version von github holen und installieren
+#
+#
+# -ansible: Erstellung bzw. Anzeige des Ansible Playbooks
+# -log: schreibt ins aktuelle Verzeichnis die Datei log.txt
+#
+
+
+
+LOG_STATUS="n";
+CHECK_STATUS="n";
+WA_STATUS="n";
+
+case $1 in
+	-h|--h|-help|--help)
+		hilfe_text;
+		echo "";
+		exit;;
+	-v|--version)
+		echo "";
+		echo "Programm:" $0;
+		echo "Version:" $VERSION "vom:" $VERSIONSDATUM
+		echo "";
+		exit;;
+	-info)
+		echo "";
+		echo "Informationen";
+		echo "=============";
+		echo "";
+		echo "Programm:" $0;
+		echo "Version:" $VERSION "vom:" $VERSIONSDATUM
+		echo "";
+		exit;;
+
+	*)
+		echo "";
+		echo "";;
+esac
