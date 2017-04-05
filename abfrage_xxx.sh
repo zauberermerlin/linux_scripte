c81c23b37545906ef7e6d30362e143945f0bc927
zauberermerlin <datensammelstelle@gmail.com>
2017-04-05 11:08:18 +0200

    Update abfrage_xxx.sh
    Finde alle *.slug-Dateien

--- abfrage_xxx.sh
+++ abfrage_xxx.sh
@@ -2,3 +2,23 @@
 #
 # Abfragen aufgrund der slug-Dateien
 #
+
+
+#######################
+#
+# Offizieller Pfad des Repos'
+# https://github.com/zauberermerlin/linux_scripte.git
+#
+# fuer curl wird der raw Pfad der einzelnen Datei benötigt
+#
+#
+#curl -L https://raw.githubusercontent.com/zauberermerlin/linux_scripte/master/abfrage_xxx.sh >abfrage_xxx.sh
+#
+#######################
+DATEN=$(find -name '*.slug';
+for i in $DATEN
+do
+  echo $i;
+done
+
+
