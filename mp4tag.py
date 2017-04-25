15da2a6ef87dc40d30de9b97a90dd47f07cf603e
zauberermerlin <datensammelstelle@gmail.com>
2017-04-25 21:42:58 +0200

    Update mp4tag.py
    Update

--- mp4tag.py
+++ mp4tag.py
@@ -1,3 +1,28 @@
-!/bin/python
+!#/usr/bin/python
 import sys
 from metaup improt MP4
+# datei = MP4("practice-makes-perfect-porn.mp4")
+# datei['\xa9nam'] = 'Eva Test'
+# datei['\xa9day'] = '2017'
+# datei['\xa9gen'] = 'Brazzers'
+# datei.save()
+# print(datei['\xa9nam'])
+# --> Fehler datei['stik'] = str(unichr(10))
+# datei['stik'] = '10'
+
+# Cover einfügen -->ok
+# with open("practice-makes-perfect-porn.jpg", "rb") as f:
+#    datei["covr"] = [
+#        MP4Cover(f.read(), imageformat=MP4Cover.FORMAT_JPEG)
+#    ]
+
+
+# datei.save()
+# print(datei['trkn'])
+# Wert 10 = tv show
+# print(datei['stik'])
+# print(datei['covr'])
+
+
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+# print 'Argument List:', str(sys.argv)
